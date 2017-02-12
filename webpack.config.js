163f2413d524765dd6db0e66574f0c71183110f4
pratik <pratik.mk.gala@gmail.com>
2017-02-12 17:04:36 -0500

    directory changes

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         port: 3000
     },
     devtool: 'cheap-module-eval-source-map',
-    entry: './dev/js/index.js',
+    entry: './client/js/index.js',
     module: {
         loaders: [
             {
