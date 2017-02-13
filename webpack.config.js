ddad4c9299484fb6913876a325cde4e81d4226c6
pratik <pratik.mk.gala@gmail.com>
2017-02-12 21:53:11 -0500

    directory changes

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     devServer: {
         inline: true,
         contentBase: './src',
-        port: 3000
+        port: 3001
     },
     devtool: 'cheap-module-eval-source-map',
     entry: './client/js/index.js',
